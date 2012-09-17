4df63fc1e5a3fdeaa03c30c9be82b95945de3905
Jorge Chiavaro <jorgechiavaro@gmail.com>
2012-09-17 12:02:35 -0300

    Adding backbone js model and views

--- hello_world.rb
+++ hello_world.rb
@@ -1,4 +1,4 @@
-require 'rubygems'
+require 'haml'
 
 # If you're using bundler, you will need to add this
 require 'bundler/setup'
@@ -6,6 +6,23 @@ require 'bundler/setup'
 require 'sinatra'
 
 get '/' do
-  "Hello world, it's #{Time.now} at the server!"
+  redirect '/hello'
 end
 
+get '/hello/:name' do
+  "Hello, #{params[:name]}"
+end
+
+get '/hello' do
+#  "Hello world, it's #{Time.now} at the server!"
+  
+  #session['counter'] ||= 0  
+  #session['counter'] += 1
+  #"You've hit this page #{session['counter']} times!"
+  haml :index
+end
+
+
+before '/hello/*' do
+  enable :sessions
+end
